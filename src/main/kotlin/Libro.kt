092f8128e7c628126c4ce7479829c217e8e2cec7
ilopjim13 <ljivan@iesrafaelalberti.es>
2024-02-14 19:42:33 +0100

    empezado en clase

--- src/main/kotlin/Libro.kt
+++ src/main/kotlin/Libro.kt
@@ -1,2 +1,3 @@
 data class Libro(val titulo :String, val autor: String, val anioPublicacion :Int) {
+    var estado:String = "Libre"
 }
--- src/main/kotlin/Usuario.kt
+++ src/main/kotlin/Usuario.kt
@@ -7,9 +7,20 @@ sealed class Usuario {
 }
 
 fun aceptarUsuario(usuario:Usuario, libro:Libro) :String {
-    return when (usuario) {
-        is Usuario.Estudiante -> "Este usuario (${usuario.nombre}) puede tomar el libro (${libro.titulo})."
-        is Usuario.Profesor -> "Este usuario (${usuario.nombre}) puede tomar el libro (${libro.titulo}) por más tiempo."
+    when (usuario) {
+        is Usuario.Estudiante -> {
+            return if (libro.estado == "Libre") {
+                libro.estado = "Ocupado"
+                "Este usuario (${usuario.nombre}) puede tomar el libro (${libro.titulo})."
+            } else "Este libro (${libro.titulo}) ya ha sido prestado."
+        }
+        is Usuario.Profesor ->{
+            return if (libro.estado == "Libre") {
+                libro.estado = "Ocupado"
+                "Este usuario (${usuario.nombre}) puede tomar el libro (${libro.titulo}) por más tiempo."
+            } else "Este libro (${libro.titulo}) ya ha sido prestado."
+        }
         is Usuario.Visitante -> "Este usuario (${usuario.nombre}) no puede tomar el libro (${libro.titulo})"
     }
+    return ""
 }
